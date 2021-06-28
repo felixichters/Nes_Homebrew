951725a58482f592f928224aed41e44f633a980b
Felix Ichters <72337408+Felauer@users.noreply.github.com>
2021-06-28 18:45:55 +0200

    added index counter(version without selecting items)

--- selfmade_tools/LvlTool/LvlTool/Main.cpp
+++ selfmade_tools/LvlTool/LvlTool/Main.cpp
@@ -1,21 +1,46 @@
 #include "Main.h"
+#include "Room.h"
 #include <iostream>
 
+#define ID_ROOM_M 10000
+#define ID_LINE_M 10001
+#define ID_RECT_M 10002
+#define ID_NAME_B 10003
+#define ID_XS_B 10004
+#define ID_YS_B 10005
+#define ID_YE_B 10006
+#define ID_XE_B 10007
+
 BEGIN_EVENT_TABLE(Main,wxFrame)
-	EVT_MENU(10001,roomClicked)
-	EVT_MENU(10002,lineClicked)
-	EVT_MENU(10003,rectClicked)
+	EVT_MENU(ID_ROOM_M,roomClicked)
+	EVT_MENU(ID_LINE_M,lineClicked)
+	EVT_MENU(ID_RECT_M,rectClicked)
+	EVT_BUTTON(ID_NAME_B,saveName )
+	EVT_BUTTON(ID_XS_B,saveXS)
+	EVT_BUTTON(ID_YS_B,saveYS)
+	EVT_BUTTON(ID_XE_B,saveXE)
+	EVT_BUTTON(ID_YE_B,saveYE)
 END_EVENT_TABLE()
 
 Main::Main() : wxFrame(nullptr, wxID_ANY, "frame", wxPoint(10,10),wxSize(800,800))
 {
+	index = 0;
 	mBar = new wxMenuBar(); 
 	add = new wxMenu();
 	objList = new wxListView(this,wxID_ANY,wxPoint(10,10),wxSize(480,750));
-	room = new wxMenuItem(add, 10001, _("&room\tCtrl+r"));
-	line = new wxMenuItem(add, 10002, _("&line\tCtrl+l"));
-	rect = new wxMenuItem(add, 10003, _("&line\tCtrl+t"));
-	nameTxt = new wxTextCtrl(this, wxID_ANY, "x", wxPoint(520, 15), wxSize(100, 20));
+	room = new wxMenuItem(add, ID_ROOM_M, _("&room\tCtrl+r"));
+	line = new wxMenuItem(add, ID_LINE_M, _("&line\tCtrl+l"));
+	rect = new wxMenuItem(add, ID_RECT_M, _("&line\tCtrl+t"));
+	nameTxt = new wxTextCtrl(this, wxID_ANY, "name", wxPoint(520, 15), wxSize(100, 20));
+	xsTxt = new wxTextCtrl(this, wxID_ANY, "x(coord)", wxPoint(520, 50), wxSize(100, 20));
+	ysTxt = new wxTextCtrl(this, wxID_ANY, "y(coord)", wxPoint(520, 75), wxSize(100, 20));
+	xeTxt = new wxTextCtrl(this, wxID_ANY, "x(start)", wxPoint(520, 105), wxSize(100, 20));
+	yeTxt = new wxTextCtrl(this, wxID_ANY, "y(end)", wxPoint(520, 130), wxSize(100, 20));
+	nameSave = new wxButton(this, ID_NAME_B, "confirm name", wxPoint(650,15),wxSize(85,20));
+	xsSave = new wxButton(this, ID_XS_B, "confirm x", wxPoint(650,50),wxSize(85,20));
+	ysSave = new wxButton(this, ID_YS_B, "confirm y", wxPoint(650, 75),wxSize(85,20));
+	xeSave = new wxButton(this, ID_XE_B, "confirm xs", wxPoint(650, 105),wxSize(85,20));
+	yeSave = new wxButton(this, ID_YE_B, "confirm ye", wxPoint(650, 130),wxSize(85,20));
 	mBar->Append(add, ("Add"));
 	add->Append(room);
 	add->Append(line);
@@ -29,20 +54,54 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "frame", wxPoint(10,10),wxSize(800,800
 	objList->AppendColumn("Y(end)");
 }
 
+//Menu Events -------------------------------------------------------------------------------------------------------
 void Main::roomClicked(wxCommandEvent& evt)
 {
-	objList->InsertItem(0,"room");
+	objList->InsertItem(index,"room");
+	index += 1;
 	evt.Skip();
 }
 
 void Main::lineClicked(wxCommandEvent& evt)
 {
-	objList->InsertItem(1, "line");
+	objList->InsertItem(index, "line");
+	index += 1;
 	evt.Skip();
 }
 
 void Main::rectClicked(wxCommandEvent& evt)
 {
-	objList->InsertItem(2, "rect");
+	objList->InsertItem(index, "rect");
+	index += 1;
 	evt.Skip();
+}
+//Button Events ------------------------------------------------------------------------------------------------------
+void Main::saveName(wxCommandEvent& evt)
+{
+	objList->SetItem(index -1 ,1, nameTxt->GetValue());
+	evt.Skip();
+}
+
+void Main::saveXS(wxCommandEvent& evt)
+{
+	evt.Skip();
+	objList->SetItem(index - 1, 1, xsTxt->GetValue());
+}
+
+void Main::saveYS(wxCommandEvent& evt)
+{
+	evt.Skip();
+	objList->SetItem(index - 1, 1, ysTxt->GetValue());
+}
+
+void Main::saveXE(wxCommandEvent& evt)
+{
+	evt.Skip();
+	objList->SetItem(index - 1, 1, xeTxt->GetValue());
+}
+
+void Main::saveYE(wxCommandEvent& evt)
+{
+	evt.Skip();
+	objList->SetItem(index - 1, 1, yeTxt->GetValue());
 }
--- selfmade_tools/LvlTool/LvlTool/Main.h
+++ selfmade_tools/LvlTool/LvlTool/Main.h
@@ -5,10 +5,13 @@
 class Main : public wxFrame
 {
 private:
+	//Main variables 
+	int index;
 	//std::vector <Room> rooms;
 
 public:
 	Main();
+	//Control
 	wxMenuBar* mBar = nullptr;
 	wxMenu* add = nullptr;
 	wxMenuItem* room = nullptr;
@@ -16,8 +19,23 @@ public:
 	wxMenuItem* rect = nullptr;
 	wxListView* objList = nullptr;
 	wxTextCtrl* nameTxt = nullptr;
+	wxTextCtrl* xsTxt = nullptr;
+	wxTextCtrl* ysTxt = nullptr;
+	wxTextCtrl* xeTxt = nullptr;
+	wxTextCtrl* yeTxt = nullptr;
+	wxButton* nameSave = nullptr;
+	wxButton* xsSave = nullptr;
+	wxButton* ysSave = nullptr;
+	wxButton* xeSave = nullptr;
+	wxButton* yeSave = nullptr;
+	//Event functions
 	void roomClicked(wxCommandEvent& evt);
 	void lineClicked(wxCommandEvent& evt);
 	void rectClicked(wxCommandEvent& evt);
+	void saveName(wxCommandEvent& evt);
+	void saveXS(wxCommandEvent& evt);
+	void saveYS(wxCommandEvent& evt);
+	void saveXE(wxCommandEvent& evt);
+	void saveYE(wxCommandEvent& evt);
 	DECLARE_EVENT_TABLE();
 };  
